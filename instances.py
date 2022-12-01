c92c6be445f1f69e4b64085bbc039d7233e64777
George Barrett <georgeaustinbarrett@gmail.com>
2022-12-01 12:44:58 +0000

    instances2.py exercise complete w comments

--- instances.py
+++ instances.py
@@ -10,12 +10,15 @@ class Parent(object):
 # this is good for repetitive code but bad for storing classes within variables
 class Child(Parent):
 
+    # because I have not typed 'pass' and instead defined a function
+    # the inheritance will be overridden
     def altered(self):
         
         # this will still print ('PARENT altered()')
         print('CHILD, BEFORE PARENT altered()')
         
         # python's super function can overide instances
+        # the .altered() fires the function that has the overridden instance
         super(Child, self).altered()
         print('CHILD, AFTER PARENT altered()')
 
